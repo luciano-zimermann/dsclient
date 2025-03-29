f9ff2de9f967013c37ee39fb21fc433c998b3b99
Luciano Zimermann <lucianozn16@gmail.com>
2025-03-28 22:01:33 -0300

    fixing client entity mapping

--- src/main/java/com/lucianozimermann/dsclient/entities/Client.java
+++ src/main/java/com/lucianozimermann/dsclient/entities/Client.java
@@ -1,12 +1,11 @@
 package com.lucianozimermann.dsclient.entities;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.time.LocalDate;
 import java.util.Objects;
 
+@Entity
 public class Client
 {
     @Id
